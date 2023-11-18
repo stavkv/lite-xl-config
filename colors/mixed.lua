eda0a4e03a844065938dad6eef7f1be8b6028bfa
vi4al <singhvikal891@gmail.com>
2023-11-18 15:15:56 +0530

    first commit

---
+++ colors/mixed.lua
@@ -0,0 +1,29 @@
+local style = require "core.style"
+local common = require "core.common"
+
+style.background = { common.color "#141b24"}
+style.background2 = { common.color "#141b24"}
+style.background3 = { common.color "#01141e"}
+style.text = { common.color "#c4c2c1"}
+style.caret = { common.color "#c5f3fd"}
+style.accent = { common.color "#78e2a0"}
+style.dim = { common.color "#a7bdc4"}
+style.divider = { common.color "#232a2d"}
+style.selection = { common.color "#232a2d"}
+style.line_number = {common.color "#5f5e60"}
+style.line_number2 = {common.color "#5f5e60"}
+style.line_highlight = {common.color "#5f5e60"}
+style.scrollbar = { common.color "#a4a7a2" }
+style.scrollbar2 = { common.color "#a4a7a2" }
+
+
+style.syntax["normal"] = { common.color "#aaced3"}
+style.syntax["symbol"] = { common.color "#67b0e8"}
+style.syntax["comment"] = {common.color "#91908f"}
+style.syntax["keyword"] = {common.color "#E67E80"}
+style.syntax["keyword2"] = {common.color "#E67E80"}
+style.syntax["number"] = {common.color "#D699B6"}
+style.syntax["literal"] = {common.color "#7FBBB3"}
+style.syntax["string"] = {common.color "#a0d8b7"}
+style.syntax["operator"] = {common.color "#DBBC7F"}
+style.syntax["function"] = {common.color "#d1b045"}
